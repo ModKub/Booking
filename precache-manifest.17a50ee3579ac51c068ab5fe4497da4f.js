6871158551a9f4d44e5e33d96c0dcbf4fd71bf16
ModKub <JakubModzelewski@gogs.storwork.pl>
2019-05-15 11:58:47 +0200

    Updates

---
+++ precache-manifest.17a50ee3579ac51c068ab5fe4497da4f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c124b4f2f85bed3407d428e452d20ce",
+    "url": "/Booking/index.html"
+  },
+  {
+    "revision": "93dd8dd36b64961d6f4f",
+    "url": "/Booking/static/css/main.bc1af6cd.chunk.css"
+  },
+  {
+    "revision": "5cbbf89e18f74322079d",
+    "url": "/Booking/static/js/2.a76cda97.chunk.js"
+  },
+  {
+    "revision": "93dd8dd36b64961d6f4f",
+    "url": "/Booking/static/js/main.0156a780.chunk.js"
+  },
+  {
+    "revision": "eb79bcaa6ace14e840a0",
+    "url": "/Booking/static/js/runtime~main.09c3bb93.js"
+  }
+]);
